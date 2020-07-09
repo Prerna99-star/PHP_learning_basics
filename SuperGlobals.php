6d5f15ee3e60dec8047bc76a30a580c4d023a96b
Prerna Mishra <59911271+Prerna99-star@users.noreply.github.com>
2020-07-09 23:52:01 +0530

    Create SuperGlobals.php
    what are SUperGlobals?

---
+++ SuperGlobals.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset = "UTF-8">
+<title></title>
+</head>
+
+<body>
+<?
+echo $_POST ['name'];
+?>
+<form method = "POST">
+   <input type="hidden" name="name" value="Prerna">
+   <button type="submit"> Press Me!! </button>
+</form>
+
+</body>
+
+</html>
